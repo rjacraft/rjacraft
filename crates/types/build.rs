3e74023293e536c946f81354e9f68c7ac714f194
Artem Popov <git@tema.sh>
2023-09-12 05:39:50 +0300

    feat(data-gen): support user-defined paths to `blocks.json`

--- crates/types/build.rs
+++ crates/types/build.rs
@@ -1,18 +1,16 @@
-fn main() {
-    use std::{fs::File, fs::OpenOptions, path::Path};
+use std::{fs::File, path::Path};
 
+fn main() {
     // Only re-run if the Minecraft JSON data files were modified.
     //println!("cargo:rerun-if-changed=../../mc-data/blocks.json");
 
-    let out_dir = std::env::var_os("OUT_DIR").expect("environment variable OUT_DIR set");
-    let mut dest_file = OpenOptions::new()
-        .write(true)
-        .create(true)
-        .truncate(true)
-        .open(Path::new(&out_dir).join("blocks.rs"))
-        .expect("create destination file");
+    let out_dir = std::env::var_os("OUT_DIR").expect("env variable OUT_DIR set");
+    let dest_path = Path::new(&out_dir).join("blocks.rs");
+    let mut dest = File::create(dest_path).expect("create destination file for blocks");
 
-    let src_path = Path::new("../../contrib/blocks.json");
-    let mut json_data = File::open(src_path).expect("read input JSON");
-    rjacraft_data_generator::gen_structs(&mut json_data, &mut dest_file).expect("generate code");
+    let blocks =
+        std::env::var_os("RJA_DATA_BLOCKS").unwrap_or_else(|| "../../contrib/blocks.json".into());
+    let mut blocks = File::open(&blocks).expect("open JSON file with blocks");
+
+    rjacraft_data_generator::gen_structs(&mut blocks, &mut dest).expect("generate code");
 }
